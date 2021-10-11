2998524b9397c4604a62e494cf25536be013395c
pranaykutthunga <90020528+pranaykutthunga@users.noreply.github.com>
2021-10-11 13:53:45 +0530

    LiionBattery.cs

---
+++ LiionBattery.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helper
+{
+    public class LiionBattery
+    {
+        public float? Temperature { get; set; }
+        public float? Soc { get; set; }
+        public float? ChargeRate { get; set; }
+    }    
+}
