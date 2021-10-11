b3f04f1097f5c8f7b1b37aff2af6213f54d491bf
pranaykutthunga <90020528+pranaykutthunga@users.noreply.github.com>
2021-10-11 13:50:00 +0530

    Helper.cs

---
+++ Helper.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helper
+{
+    internal class Helper
+    {     
+        LiionBattery _objLiionBattery = new LiionBattery();
+        
+        public Helper(float temp, float soc, float chargeRate)
+        {
+            _objLiionBattery.Temperature = temp;
+            _objLiionBattery.Soc = soc;
+            _objLiionBattery.ChargeRate = chargeRate;
+        }
+
+        public bool IsChargeRateInRange(float chargeRate)
+        {
+            try
+            {
+              if (chargeRate > 0.8)
+                return false;
+              else
+                return true;
+            }
+            catch(Exception ex)
+            {
+              throw ex;
+            }            
+        }
+
+        private bool IsSOCInRange(float soc)
+        {
+            try
+            {
+              if (soc < 20 || soc > 80)
+                return false;
+              else
+                return true;
+            }
+            catch(Exception ex)
+            {
+              throw ex;
+            }  
+        }
+
+        private bool IsTempInRange(double temperature)
+        {
+            try
+            {
+              if (temperature < 0 || temperature > 45)
+                return false;
+            else
+                return true;
+            }
+            catch(Exception ex)
+            {
+              throw ex;
+            }  
+        }
+        
+        private void OutputInformation(string message)
+        {
+            try
+            {
+              Console.WriteLine(message);
+            }
+            catch(Exception ex)
+            {
+              throw ex;
+            }
+        }
+        internal bool BatteryIsOk()
+        {
+            try
+            {
+              if (!IsTempInRange(_objLiionBattery.Temperature))
+              {
+                  OutputInformation("Temperature is out of range!");
+                  return false;
+              }
+              else if (!IsSOCInRange(_objLiionBattery.Soc))
+              {
+                  OutputInformation("State of Charge is out of range!");
+                  return false;
+              }
+              else if (!IsChargeRateInRange(_objLiionBattery.ChargeRate))
+              {
+                  OutputInformation("Charge Rate is out of range!");
+                  return false;
+              }
+            }
+            catch(Exception ex)
+            {
+              throw ex;
+            }
+            return true;
+        }
+    }
+}
