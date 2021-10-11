c0a2b4662a970c273597a36bd2b81cf38915d809
pranaykutthunga <90020528+pranaykutthunga@users.noreply.github.com>
2021-10-11 14:03:49 +0530

    Update LiionBattery.cs

--- LiionBattery.cs
+++ LiionBattery.cs
@@ -7,8 +7,8 @@ namespace Helper
 {
     public class LiionBattery
     {
-        public float? Temperature { get; set; }
-        public float? Soc { get; set; }
-        public float? ChargeRate { get; set; }
+        public float Temperature { get; set; }
+        public float Soc { get; set; }
+        public float ChargeRate { get; set; }
     }    
 }
